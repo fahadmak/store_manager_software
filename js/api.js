5c63aed1e2433dcdf12e13b9c43bfbb1cbe4b9e4
faraqhan <f.faraqhan@gmail.com>
2018-11-14 16:05:37 +0300

    create variables + add event listener for login button[#161953968]

--- js/api.js
+++ js/api.js
@@ -1,3 +1,11 @@
+const login_url = 'http://127.0.0.1:5000/api/v1/auth/login';
+
+let merrortext = document.querySelector('#merrortext');
+let uerrortext = document.querySelector('#uerrortext');
+let perrortext = document.querySelector('#perrortext');
+
+document.getElementById('logs').addEventListener('click', login);
+
 function load(url) {
     location.href = url;
 }
