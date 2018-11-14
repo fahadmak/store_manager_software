2cc8006e067e8aaad0d0bbc131582f4507e3efc5
faraqhan <f.faraqhan@gmail.com>
2018-11-14 16:09:40 +0300

    add fetch api and error handlers [#161953968]

--- js/api.js
+++ js/api.js
@@ -9,7 +9,62 @@ document.getElementById('logs').addEventListener('click', login);
 function login() {
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
-
+    fetch(login_url, {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'content-type': 'application/json'
+    },
+    body:JSON.stringify({username:username, password:password})
+    })
+    .then(handleResponse)
+    .then((data) => {
+        if (data.admin === true){
+            localStorage.setItem('token', data.access_token);
+            console.log(localStorage.getItem('token'));
+            load('admin/viewproducts.html');
+        } else {
+            localStorage.setItem('token', data.access_token);
+            console.log(localStorage.getItem('token'));
+            load('attendant/shopping_cart.html');
+        }})
+    .catch(function (error) {
+        if (error.error === 'Username and password did not match'){
+            merrortext.style.display = 'block';
+            merrortext.innerText = 'Password should contain at least 4 letters and a number';
+            setTimeout(function () {
+                merrortext.style.display = 'none'
+            }, 5000)
+        } else {
+            if (error.password){
+                perrortext.style.display = 'block';
+                perrortext.innerText = 'Password should contain at least 4 letters and a number';
+                setTimeout(function () {
+                perrortext.style.display = 'none'
+            }, 5000)
+            }
+            if (error.username){
+                uerrortext.style.display = 'block';
+                uerrortext.innerText = 'Username should contain at least 4 letters and a number';
+                setTimeout(function () {
+                uerrortext.style.display = 'none'
+            }, 5000)
+            }
+            if (error.username && error.password){
+                uerrortext.style.display = 'block';
+                uerrortext.innerText = 'Username should contain at least 4 letters and a number';
+                setTimeout(function () {
+                uerrortext.style.display = 'none'
+            }, 5000);
+                perrortext.style.display = 'block';
+                perrortext.innerText = 'Password should contain at least 4 letters and a number';
+                setTimeout(function () {
+                perrortext.style.display = 'none'
+            }, 5000)
+            }
+            console.log('Request failed', error);
+        }
+    });
 }
 
 function load(url) {
