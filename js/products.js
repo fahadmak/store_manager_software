989860c56b516ae808ce5bb17709358940a85f74
faraqhan <f.faraqhan@gmail.com>
2018-11-14 16:41:17 +0300

    create response object [#161955922]

--- js/products.js
+++ js/products.js
@@ -5,3 +5,14 @@ var myHeaders = new Headers({
         'content-type': 'application/json',
         'Authorization' : 'Bearer '  + localStorage.getItem('token'),
     });
+
+var myInit = {
+    method: 'GET',
+    headers: myHeaders,
+    cache: 'default',
+    mode: 'cors'
+};
+
+
+const product_url = 'http://127.0.0.1:5000/api/v1/products';
+const myRequest = new Request(product_url, myInit);
