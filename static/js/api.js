ecfed5fdafa0cde7d4f1e6d275183f67559f486a
fahadmak <f.faraqhan@gmail.com>
2018-11-29 08:57:15 +0300

    Update api.js

--- static/js/api.js
+++ static/js/api.js
@@ -1,4 +1,4 @@
-const login_url = 'http://127.0.0.1:5000/api/v1/auth/login';
+const login_url = 'https://store-challenge-3-api.herokuapp.com/api/v1/auth/login';
 
 let merrortext = document.querySelector('#merrortext');
 let uerrortext = document.querySelector('#uerrortext');
